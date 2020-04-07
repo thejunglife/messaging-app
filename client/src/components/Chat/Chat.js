9ba9f68260ab087603336abf0a77cc5562747124
Paul <thejunglife@gmail.com>
2020-04-07 03:41:58 -0700

    chat functionality working

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,6 +9,8 @@ let socket
 const Chat = ({ location }) => {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
     const ENDPOINT = 'localhost:5000'
 
     useEffect(() => {
@@ -30,8 +32,34 @@ const Chat = ({ location }) => {
             socket.off()
         }
     }, [ENDPOINT, location.search])
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+
+    // function for sending messages
+
+    const sendMessage = (event)  => {
+        event.preventDefault()
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    console.log(message, messages)
+    
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input 
+                value={message} 
+                onChange={(event) => setMessage(event.target.value)} 
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null }
+                />
+            </div>
+        </div>
     )
 }
 
